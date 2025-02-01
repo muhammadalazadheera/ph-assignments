03bdb89e0eb6b2cbcbc5259bc95338d1e33739d4
Al Azad Heera <alazadheera@gmail.com>
2025-02-01 12:01:52 +0600

    hero section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     extend: {
       colors: {
         solid:'#FFDC26'
+      },
+      backgroundImage: {
+        'hero-image': "url('../images/cat-big.png')"
+      },
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '8rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+      height: {
+        'hero-height': "calc(100vh - 70px)"
       }
     },
   },
