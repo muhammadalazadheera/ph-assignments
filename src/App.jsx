f30911127ac8ee3e59cb0776b4c9b0ac0222dc15
Al Azad Heera <alazadheera@gmail.com>
2025-04-08 21:27:42 +0600

    React toastify package added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ToastContainer, toast } from 'react-toastify'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const notify = () => toast("Wow so easy!");
   return (
     <>
       <div>
@@ -28,6 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button onClick={notify}>Notify!</button>
+        <ToastContainer />
     </>
   )
 }
